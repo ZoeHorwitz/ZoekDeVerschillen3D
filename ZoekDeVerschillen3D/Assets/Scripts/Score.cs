2bdd832c83349370d096591009db9bedcfe499e7
ZoeHorwitz <99327981+ZoeHorwitz@users.noreply.github.com>
2022-04-11 19:15:40 +0200

    score code + UI

--- ZoekDeVerschillen3D/Assets/Scripts/Score.cs
+++ ZoekDeVerschillen3D/Assets/Scripts/Score.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Score : MonoBehaviour
 {
     public int score;
     public int wrong;
+    public TextMeshProUGUI scoreTextWrong;
+    public TextMeshProUGUI scoreTextRight;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +20,11 @@ public class Score : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        scoreTextWrong.text = " " + wrong;
+        if (wrong > 2)
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+        scoreTextRight.text = " " + score;
     }
 }
